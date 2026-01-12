f4e1edd11478a857ba9808550c127fb3e05dba8d
Ahmed Rabie <ahmed.r@trusstedge.com>
2026-01-12 21:06:59 +0200

    feat(core): add transform response interceptor to unify api output

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,14 +3,21 @@ import { DatabaseModule } from '../database/database.module';
 import { LoggerService } from './logger/logger.service';
 import { ConfigModule } from '@nestjs/config';
 import config from '../config';
-
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformResponseInterceptor } from './interceptors/transform-response/transform-response.interceptor';
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     DatabaseModule,
   ],
-  providers: [LoggerService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformResponseInterceptor,
+    },
+    LoggerService,
+  ],
   exports: [DatabaseModule, LoggerService],
 })
 export class CoreModule {}
