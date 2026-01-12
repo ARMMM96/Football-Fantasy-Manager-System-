67b54856c3c4e7c87421db9ac94e36aa45bdccea
Ahmed Rabie <ahmed.r@trusstedge.com>
2026-01-12 23:54:31 +0200

    chore: add redis dependencies and update config to support redis

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,10 @@
 export default () => ({
+  host: process.env.HOST,
   environment: process.env.NODE_ENV || `development`,
+  redis: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+  },
 });
